af9e9c12bea3bb033387cc2314c9f3a4015a88b9
Marcelino Briseno <mbrisenoj@gmail.com>
2015-07-22 19:54:13 -0700

    Initial COmmit

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port);
+});
+
+app.get("/", function(req, res) {
+  res.send('Hello!!');
+});
+
+app.get("/adjectives", function(req, res) {
+  res.json(getRandomWord(adjectives));
+});
+
+function Adjectives(){
+  this.cute = true;
+  this.kawaii = true;
+  this.tensai = true;
+  this.dynamite = true;
+}
+
+var adjectives = new Adjectives();
+
+function getRandomWord(object) {
+  var propArray = Object.keys(object);
+  var randomProp = propArray[Math.floor(Math.random() * propArray.length)];
+  return {word: randomProp};
+}
